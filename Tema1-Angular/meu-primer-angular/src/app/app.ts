4f7187eb7658bf59a99c03e1b707aa21cc4ece05
albert-dev <al96@hotmail.es>
2026-06-22 12:09:38 +0200

    feature: "modified component to show change the title when you click a button"

--- Tema1-Angular/meu-primer-angular/src/app/app.ts
+++ Tema1-Angular/meu-primer-angular/src/app/app.ts
@@ -7,6 +7,10 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('meu-primer-angular');
+
+export class AppComponent {
+  title = signal('Món Angular');
+  changeTitle() {
+    this.title.set('Títol Canviat per Signal!');
+  }
 }
